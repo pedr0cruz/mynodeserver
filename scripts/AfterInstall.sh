8603a8b9f5fed4b214b463454a34f7ff1cd5b819
Pedro Cruz <pcruzh@gmail.com>
2022-03-06 22:57:04 -0500

    cambiar a ec2-user

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-cd /home/app/myNodeServer
+cd /home/ec2-user/myNodeServer
 npm install
 # npm run build
--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -3,6 +3,6 @@ set -e
 sudo yum update -y
 # pm2 update
 
-sudo rm -rf /home/app/myNodeServer
-cd /home/app
+sudo rm -rf /home/ec2-user/myNodeServer
+cd /home/ec2-user
 mkdir myNodeServer
